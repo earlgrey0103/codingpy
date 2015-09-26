6060ae5380901ddcfdde4c03a30a5a0a215b29d0
bingjin <545915891@qq.com>
2015-09-26 15:25:47 +0000

    index page

--- codingpy/controllers/admin.py
+++
@@ -1,7 +0,0 @@
-#!usr/bin/env python
-# -*- coding: utf-8 -*-
-from flask import Blueprint
-from flask import render_template, redirect, url_for, flash, request,\
-    current_app
-
-bp = Blueprint('admin', __name__)
