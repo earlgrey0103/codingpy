a670e2aba47d02a9422b076b186e8e06a2000cae
bingjin <545915891@qq.com>
2015-10-15 01:54:17 +0000

    improvements

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from gevent.wsgi import WSGIServer
 from manage import app as application
 
 if __name__ == '__main__':
+    # http_server = WSGIServer(('', 5000), application)
+    # http_server.serve_forever()
     application.run()
